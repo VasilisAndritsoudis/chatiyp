5f7c56db7e02a94e43e5246b969fc6145feb3c48
Vasilis Andritsoudis <vasilisandritsoudis@gmail.com>
2025-08-03 01:59:33 +0300

    UI improvements

--- app.py
+++ app.py
@@ -5,31 +5,30 @@ from retriever import init_chatiyp_model
 
 st.set_page_config(page_title="ChatIYP", layout="wide")
 
-# Initialize session state key ONCE
-if "api_key" not in st.session_state:
-    st.session_state.api_key = None
-
 if "OPENAI_API_KEY" not in os.environ:
-    st.title("🔐 Enter your OpenAI API Key")
-    api_key_input = st.text_input("API Key")
-    if api_key_input:
-        os.environ["OPENAI_API_KEY"] = api_key_input
-        st.rerun()  # Reload app with key stored
-    st.stop()  # Prevent rest of app from running
-else:
-    st.markdown(
-        f"✅ **Using OpenAI API Key:** `{os.environ["OPENAI_API_KEY"][:6]}...{os.environ["OPENAI_API_KEY"][-4:]}`"
-    )
+    os.environ["OPENAI_API_KEY"] = ""
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "ai", "content": "Hello, what can I help with?"}]
 
 # Title and description
-st.title("🤖 ChatIYP")
-st.markdown(
-    "🔎 A GPT-3.5-Turbo-powered chatbot that leverages Retrieval-Augmented Generation (RAG) to answer questions on the Internet Yellow Pages (IYP) knowledge graph."
-)
+st.title("🌐🤖 ChatIYP: Enabling natural language access to the Internet Yellow Pages")
+st.info("🔎 This is a demo of a GPT-3.5-Turbo-powered chatbot that leverages Retrieval-Augmented Generation (RAG) to answer questions on the Internet Yellow Pages (IYP) knowledge graph.")
+
+st.markdown("""
+### Some examples queries you can test or get inspired
+- What is the name of AS2497
+- Which ASN originates the prefix 8.8.8.0/24
+- What is the country and name of AS2497
+- Countries of IXPs where AS2497 is present
+- Select domain names that resolve to an IP originated by AS5470
+- Select AS dependencies for AS5470
+""")
+
+st.warning("⚠️ The demo is in experimental phase. Usually it works well for questions similar to the above examples. For custom questions (especially too complex or too abstract) it may have problems in generating a good answer.")
+
+st.warning("⚠️ Currently (experimental phase), the answers of the AI agent are limited to 10 items (e.g., if you ask which prefixes does an ASN originates, the answer will contain at most 10 prefixes)")
 
 # Connect to Neo4j only once
 if "query_engine" not in st.session_state or "sub_retriever" not in st.session_state:
@@ -58,16 +57,22 @@ if prompt:
     with st.chat_message("user"):
         st.write(prompt)
 
-    # Generate assistant response
-    with st.status("Generating response...", expanded=True) as status:
-        answer = str(st.session_state.query_engine.query(prompt))
-        cypher = st.session_state.sub_retriever.generated_cypher_query
-        status.update(label="Generated.", state="complete", expanded=False)
-
-    # Store and display assistant message
-    st.session_state.messages.append({"role": "ai", "content": answer, "cypher": cypher})
     with st.chat_message("ai"):
-        st.write(answer)
-        with st.expander("Show Cypher query"):
-            st.code(cypher, language="cypher")
+        message_placeholder = st.empty()
+        expander_placeholder = st.empty()
+        try:
+            # Generate assistant response
+            with st.spinner("Generating response..."):
+                answer = str(st.session_state.query_engine.query(prompt))
+                cypher = st.session_state.sub_retriever.generated_cypher_query
+
+            # Store and display assistant message
+            st.session_state.messages.append({"role": "ai", "content": answer, "cypher": cypher})
+            message_placeholder.write(answer)
+            with expander_placeholder.expander("Show Cypher query"):
+                st.code(cypher, language="cypher")
+        except:
+            error_message = f"⚠️ An error occurred while generating a response."
+            st.session_state.messages.append({"role": "ai", "content": error_message})
+            message_placeholder.write(error_message)
         
